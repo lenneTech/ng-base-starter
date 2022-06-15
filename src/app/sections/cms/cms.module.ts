d7926c1eb7a32838baefa615833caaaf59bbd603
Pascal Klesse <pascal.klesse@lenne.tech>
2022-06-16 01:46:58 +0200

    feat: Update to ng-base 13.18.0

--- src/app/sections/cms/cms.module.ts
+++ src/app/sections/cms/cms.module.ts
@@ -4,7 +4,39 @@ import { BaseCMSModule } from '@lenne.tech/ng-base/base-cms';
 
 BaseCMSModule.config = {
   branding: true,
-  restrictedModels: [{ model: 'user', roles: ['admin'] }],
+  logging: false,
+  modelConfig: {
+    user: {
+      label: 'Nutzer',
+      plural: 'Nutzer',
+      excluded: false,
+      restricted: true,
+      roles: ['admin'],
+    },
+  },
+  fieldConfig: {
+    user: {
+      email: {
+        label: 'E-Mail',
+        placeholder: '',
+      },
+      firstName: {
+        label: 'Vorname',
+      },
+      lastName: {
+        label: 'Nachname',
+      },
+      roles: {
+        label: 'Rollen',
+      },
+      username: {
+        label: 'Benutzername',
+      },
+      password: {
+        exclude: true,
+      },
+    },
+  },
 };
 
 @NgModule({
