32b93278b15b58dc647ae79530d9df85ea5e229e
Pascal Klesse <pascal.klesse@lenne.tech>
2022-09-16 22:16:55 +0200

    Add Phpstorm settings and update to ng-bas 14.12.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,15 @@
 export const environment = {
   production: true,
+  logoPath: 'https://de.expensereduction.com/wp-content/uploads/2018/02/logo-placeholder.png',
 
   // Settings for @lenne.tech/ng-base
   prefix: 'app',
-  // apiUrl: '',
-  // wsUrl: '',
-  // authGuardRedirectUrl: '/auth',
-  // logging: false,
+  apiUrl: 'http://127.0.0.1:3000/graphql',
+  wsUrl: 'ws://127.0.0.1:3000/graphql',
+  restUrl: 'http://127.0.0.1:3000',
+  appUrl: 'http://127.0.0.1:4200',
+  authGuardRedirectUrl: '/auth',
+  logging: false,
   // version: '0.0.1',
   // scrollDetectionOffset: 200,
   // scrollOffset: 100,
