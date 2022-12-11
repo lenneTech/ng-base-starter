c6ce34638492648e309890db286de9aecd5fafaf
Kai Haase <kai.haase@lenne.tech>
2022-12-11 18:39:39 +0100

    Fixed missing BASE_MODULE_CONFIG in HeaderComponent test

--- src/app/modules/shared/components/header/header.component.spec.ts
+++ src/app/modules/shared/components/header/header.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BASE_MODULE_CONFIG } from '@lenne.tech/ng-base/shared';
+import { environment } from '../../../../../environments/environment';
 
 import { HeaderComponent } from './header.component';
 
@@ -8,6 +10,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [{ provide: BASE_MODULE_CONFIG, useValue: environment }],
       declarations: [HeaderComponent],
     }).compileComponents();
 
